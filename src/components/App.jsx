584eb166d521e47e00a74b6b87a7110824953b44
Jatsekku <jatsekku2@gmail.com>
2025-02-08 02:06:55 +0100

    feat(components): Add rand dice generation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,20 @@
 import Die from './Die'
+import { useState } from 'react'
 
 export default function App() {
+
+    function generateAllNewDice() {
+        return Array.from({length: 10}, () => Math.ceil(Math.random() * 6))
+    }
+
+    const [dice, setDice] = useState(generateAllNewDice())
+    console.log(dice)
+    
+    const diceElements = dice.map(num => <Die value={num} />)
+
     return <main>
         <div className='dice-container'>
-            <Die value={1} />
-            <Die value={1} />
-            <Die value={1} />
-            <Die value={1} />
-            <Die value={1} />
-
-            <Die value={1} />
-            <Die value={1} />
-            <Die value={1} />
-            <Die value={1} />
-            <Die value={1} />
+            {diceElements}
         </div>
     </main>
 }
